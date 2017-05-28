2b6a95836ae323bf3e08e0297bbf46be9298d552
rogelio1321 <hardy1066@hotmail.com>
2017-05-28 17:06:08 -0600

    bootstrap

--- application/views/persona/vpersona.php
+++ application/views/persona/vpersona.php
@@ -1,45 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Registrar</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/iCheck/square/blue.css">
 
-<h1>Cargo Persona</h1>
-<form action="<?php echo base_url(); ?>cpersona/guardar" method="POST">
-	<table>
-		<tr>
-		   <td><label>DNI</label></td>
-		   <td><input type="text" name="txtDNI" maxlength="8"></td>	
-		</tr>
-		<tr>
-		   <td><label>Nombre</label></td>
-		   <td><input type="text" name="txtNombre"></td>	
-		</tr>
-		<tr>
-		   <td><label>Apellido Paterno</label></td>
-		   <td><input type="text" name="txtApPaterno"></td>	
-		</tr>
-		<tr>
-		   <td><label>Apellido Materno</label></td>
-		   <td><input type="text" name="txtApMaterno"></td>	
-		</tr>
-		<tr>
-		   <td><label>Email</label></td>
-		   <td><input type="text" name="txtEmail"></td>	
-		</tr>
-		<tr>
-		   <td><label>Fec. Nac.</label></td>
-		   <td><input type="date" name="datFecNac"></td>	
-		</tr>
-		<tr>
-		   <td colspan="2"><label>Usuario</label></td>
-		<tr>
-		   <td><label>Usuario</label></td>
-		   <td><input type="text" name="txtUsuario"></td>	
-		</tr>
-		<tr>
-		   <td><label>Clave</label></td>
-		   <td><input type="password" name="txtClave"></td>	
-		</tr>
-		<tr>
-		   <td colspan="2"><input type="submit" value="Guardar"></td>
-		   	
-		</tr>
-	</table>
-	</form>
-	<a href="<?php echo base_url();?>clogin">Login</a>		
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="../../index2.html"><b>Sign</b>IN</a>
+  </div>
+
+  <div class="register-box-body">
+    <p class="login-box-msg">Register a new membership</p>
+
+   <form action="<?php echo base_url(); ?>cpersona/guardar" method="POST">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="txtDNI" maxlength="8" placeholder="DNI">
+      </div>
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="txtNombre" placeholder="Nombre">
+      </div>
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="txtApPaterno" placeholder="Apellido Paterno">
+        
+      </div>
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="txtApMaterno" placeholder="Apellido Materno">
+        
+      </div>
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" name="txtEmail" placeholder="Email">
+        
+      </div>
+      <div class="form-group has-feedback">
+        <input type="date" class="form-control" name="datFecNac" placeholder="Fec. Nac">
+        
+      </div>
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="txtUsuario" placeholder="Usuario">
+        
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" name="txtClave" placeholder="Contraseña">
+       
+     </div>
+      <div class="row">
+        
+        <!-- /.col --> 
+        <div class="col-xs-4">
+        	<script>
+        	function abrir(){
+        		window.open("<?php echo base_url();?>clogin")
+        	}
+        	</script>
+          <button type="submit" onclick="abrir()" class="btn btn-primary btn-block btn-flat">Register</button>
+
+
+        </div>
+        <!-- /.col -->
+      </div>
+      <a href="<?php echo base_url();?>clogin" class="text-center">Log in</a>
+    </form>
+
+    
+<!-- /.register-box -->
+
+<!-- jQuery 2.2.3 -->
+<script src="<?php echo base_url();?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url();?>assets/plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
+
